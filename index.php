7339f5e8672a7edb4988298c814a95bada67db6b
ieheng <34223968+iecheng@users.noreply.github.com>
2018-01-06 18:15:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+require_once('memobird.php');
+$memobird = new memobird();
+$memobird = '';//设备编码
+$str = '';//用户标识
+//绑定并获取用户ID
+$res = $memobird->getUserId($memobirdID, $str);
+$userid = $res['showapi_userid'];
+//添加打印文字
+$c = $memobird->addTextContent('欢迎使用MEMOBIRD');
+//添加图片
+$c = $memobird->addImagesContent(file_get_contents('example.bmp'));
+//获取单色图片
+$res = $memobird->getPic(base64_encode(file_get_contents('example.jpg')));
+$c = $memobird->addContent('P:' . $res['result']);
+//打印
+$res = $memobird->printPaper($memobird, $userid);
+$url = 'http://memobird.iecheng.cn/tmp/memobird/example.html';
+$res = $memobird->printUrl($url, $memobird, $userid);
+$html = '<html><head></head><style>h2{font-size:2em;}</style><body><h2>欢迎使用</h2><p>Hello,World!</p></body></html>';
+$res = $memobird->printHtml($html, $memobird, $userid);

487a8256da99e9cefeb355aad1aee4e6e1c4db21
ieheng <34223968+iecheng@users.noreply.github.com>
2018-01-06 19:38:59 +0800

    bug fixed $memobirdid

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require_once('memobird.php');
 $memobird = new memobird();
-$memobird = '';//设备编码
+$memobirdid = '';//设备编码
 $str = '';//用户标识
 //绑定并获取用户ID
-$res = $memobird->getUserId($memobirdID, $str);
+$res = $memobird->getUserId($memobirdid, $str);
 $userid = $res['showapi_userid'];
 //添加打印文字
 $c = $memobird->addTextContent('欢迎使用MEMOBIRD');
@@ -14,8 +14,8 @@ $c = $memobird->addImagesContent(file_get_contents('example.bmp'));
 $res = $memobird->getPic(base64_encode(file_get_contents('example.jpg')));
 $c = $memobird->addContent('P:' . $res['result']);
 //打印
-$res = $memobird->printPaper($memobird, $userid);
+$res = $memobird->printPaper($memobirdid, $userid);
 $url = 'http://memobird.iecheng.cn/tmp/memobird/example.html';
-$res = $memobird->printUrl($url, $memobird, $userid);
+$res = $memobird->printUrl($url, $memobirdid, $userid);
 $html = '<html><head></head><style>h2{font-size:2em;}</style><body><h2>欢迎使用</h2><p>Hello,World!</p></body></html>';
-$res = $memobird->printHtml($html, $memobird, $userid);
+$res = $memobird->printHtml($html, $memobirdid, $userid);

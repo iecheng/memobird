03e6220912764de7616c5aff9cb222dd022c11b0
ieheng <34223968+iecheng@users.noreply.github.com>
2018-01-06 19:43:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <?php
 require_once('memobird.php');
+date_default_timezone_set('Etc/GMT-8');
 $memobird = new memobird();
-$memobirdid = '';//设备编码
-$str = '';//用户标识
+$memobirdID = '';//设备编码
+$str = 'default';//用户标识
 //绑定并获取用户ID
-$res = $memobird->getUserId($memobirdid, $str);
+$res = $memobird->getUserId($memobirdID, $str);
 $userid = $res['showapi_userid'];
+var_dump($res);
 //添加打印文字
 $c = $memobird->addTextContent('欢迎使用MEMOBIRD');
 //添加图片
 $c = $memobird->addImagesContent(file_get_contents('example.bmp'));
 //获取单色图片
 $res = $memobird->getPic(base64_encode(file_get_contents('example.jpg')));
+var_dump($res);
 $c = $memobird->addContent('P:' . $res['result']);
 //打印
-$res = $memobird->printPaper($memobirdid, $userid);
+$res = $memobird->printPaper($memobirdID, $userid);
+var_dump($res);
 $url = 'http://memobird.iecheng.cn/tmp/memobird/example.html';
-$res = $memobird->printUrl($url, $memobirdid, $userid);
+$res = $memobird->printUrl($url, $memobirdID, $userid);
+var_dump($res);
 $html = '<html><head></head><style>h2{font-size:2em;}</style><body><h2>欢迎使用</h2><p>Hello,World!</p></body></html>';
-$res = $memobird->printHtml($html, $memobirdid, $userid);
+$res = $memobird->printHtml($html, $memobirdID, $userid);
+var_dump($res);
